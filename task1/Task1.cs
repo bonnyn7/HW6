b6eecd145f2b1ef63141a6f3948e983a8214a323
Anastasia <bonnyn7@yandex.ru>
2022-07-16 14:18:09 +0300

    Added tasks

---
+++ task1/Task1.cs
@@ -0,0 +1,36 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using System;
+namespace Homework6;
+class Task1
+{
+    public static void Main (string[] args)
+    {
+        Console.Write($"Введите число М (количество чисел): ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        int[] arrayNumbers = new int[m];
+
+        void InputNumbers(int m)
+        {
+            for (int i = 0; i < m; i++)
+            {
+                Console.Write($"Введи {i + 1} число: ");
+                arrayNumbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        int Compare (int[] arrayNumbers)
+        {
+            int count = 0;
+            for (int i = 0; i < arrayNumbers.Length; i++)
+            {
+                if (arrayNumbers[i] > 0) count += 1;
+            }
+            return count;
+        }
+        InputNumbers (m);
+        Console.WriteLine($"Введено чисел больше 0: {Compare (arrayNumbers)} ");
+    }
+}
---
+++ task2/Task2.cs
@@ -0,0 +1,56 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+using System;
+
+namespace Homework6;
+
+class Task2
+{
+    public static void Main(string[] args)
+    {
+        double[,] ratio = new double[2, 2];
+        double[] crossPoint = new double[2];
+
+        void InputRatios()
+        {
+            for (int i = 0; i < ratio.GetLength(0); i++)
+            {
+                Console.Write($"Введите коэффициенты {i + 1}-го уравнения (y = k * x + b):\n");
+                for (int j = 0; j < ratio.GetLength(1); j++)
+                {
+                    if (j == 0) Console.Write($"Введите коэффициент k: ");
+                    else Console.Write($"Введите коэффициент b: ");
+                    ratio[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+
+        double[] Decision(double[,] ratio)
+        {
+            crossPoint[0] = (ratio[1, 1] - ratio[0, 1]) / (ratio[0, 0] - ratio[1, 0]);
+            crossPoint[1] = crossPoint[0] * ratio[0, 0] + ratio[0, 1];
+            return crossPoint;
+        }
+
+        void OutputResponse(double[,] ratio)
+        {
+            if (ratio[0, 0] == ratio[1, 0] && ratio[0, 1] == ratio[1, 1])
+            {
+                Console.Write($"\nПрямые совпадают");
+            }
+            else if (ratio[0, 0] == ratio[1, 0] && ratio[0, 1] != ratio[1, 1])
+            {
+                Console.Write($"\nПрямые параллельны");
+            }
+            else
+            {
+                Decision(ratio);
+                Console.Write($"\nТочка пересечения прямых: ({crossPoint[0]}, {crossPoint[1]})");
+            }
+        }
+
+        InputRatios();
+        OutputResponse(ratio);
+    }
+}
